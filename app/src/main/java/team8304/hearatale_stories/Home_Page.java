7660d3852182ae968251dfa3166a218a0912f78f
Manu Poruthoor <manu.poruthoor@gatech.edu>
2019-01-16 21:39:35 -0500

    library screen v1

--- app/src/main/java/team8304/hearatale_stories/Home_Page.java
+++ app/src/main/java/team8304/hearatale_stories/Home_Page.java
@@ -1,7 +1,11 @@
 package team8304.hearatale_stories;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home_Page extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class Home_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__page);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        Button but = findViewById(R.id.buttonqxj);
+        but.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Home_Page.this, LibraryActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
