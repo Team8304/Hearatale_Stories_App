6b24e01c543595504ccfbdbcd8d96d74f5886417
Nick Angalet <nangalet@gmail.com>
2019-01-16 21:47:07 -0500

    Revert "library screen v1"
    This reverts commit 7660d3852182ae968251dfa3166a218a0912f78f.

--- app/src/main/java/team8304/hearatale_stories/Home_Page.java
+++ app/src/main/java/team8304/hearatale_stories/Home_Page.java
@@ -1,11 +1,7 @@
 package team8304.hearatale_stories;
 
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class Home_Page extends AppCompatActivity {
 
@@ -13,13 +9,5 @@ public class Home_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__page);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        Button but = findViewById(R.id.buttonqxj);
-        but.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(Home_Page.this, LibraryActivity.class);
-                startActivity(i);
-            }
-        });
     }
 }
