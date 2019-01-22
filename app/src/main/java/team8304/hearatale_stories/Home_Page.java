987c85b9a0f253c9e1d821dc9df6f43bd4a9147b
Kevin Nguyen <Kevinnguyen55@users.noreply.github.com>
2019-01-21 20:29:59 -0500

    Added Splash Screen and code to connect homepage

--- app/src/main/java/team8304/hearatale_stories/Home_Page.java
+++ app/src/main/java/team8304/hearatale_stories/Home_Page.java
@@ -1,7 +1,9 @@
 package team8304.hearatale_stories;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home_Page extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Home_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__page);
+    }
+
+    public void navigateToLibrary(View view) {
+//        Intent startLibraryActivity = new Intent(this, Library.class);
+//        startActivity(startLibraryActivity);
     }
 }
