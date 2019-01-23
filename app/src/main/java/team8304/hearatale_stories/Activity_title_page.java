fcf9ccbed7a2aad9ca5400e7d557aa9b86cd5a23
wsxplmm123 <554989106@qq.com>
2019-01-22 22:35:05 -0500

    merge

---
+++ app/src/main/java/team8304/hearatale_stories/Activity_title_page.java
@@ -0,0 +1,13 @@
+package team8304.hearatale_stories;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_title_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_page);
+    }
+}
