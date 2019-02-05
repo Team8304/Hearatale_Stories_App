07440617abcd8b4b5f3316076fd5beaf17be4281
Kevin Nguyen <Kevinnguyen55@users.noreply.github.com>
2019-02-05 17:45:54 -0500

    Added the book screen with pause/play and progress bar

---
+++ app/src/main/java/team8304/hearatale_stories/BookActivity.java
@@ -0,0 +1,118 @@
+package team8304.hearatale_stories;
+
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class BookActivity extends AppCompatActivity {
+
+    Button playButton;
+    SeekBar seekBar;
+    TextView elapsedTimeLabel;
+    TextView remainingTimeLabel;
+    MediaPlayer mp;
+    int totalTime;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+
+        playButton = (Button) findViewById(R.id.playButton);
+        elapsedTimeLabel = (TextView) findViewById(R.id.elapsedTimeLabel);
+        remainingTimeLabel = (TextView) findViewById(R.id.remainingTimeLabel);
+
+        //RawMusic is where the title searches for in the specific folder
+        mp = MediaPlayer.create(this, R.raw.test);
+        mp.setLooping(false);
+        mp.seekTo(0);
+        mp.setVolume(0.5f, 0.5f);
+        totalTime = mp.getDuration();
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setMax(totalTime);
+        seekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekbar, int progress, boolean fromUser) {
+                        if (fromUser) {
+                            mp.seekTo(progress);
+                            seekBar.setProgress(progress);
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+        //Thread (Update seekbar and Time labels)
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mp != null) {
+                    try {
+                        Message msg = new Message();
+                        msg.what = mp.getCurrentPosition();
+                        handler.sendMessage(msg);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int currentPosition = msg.what;
+            seekBar.setProgress(currentPosition);
+
+            String elapsedTime = createTimeLabel(currentPosition);
+            elapsedTimeLabel.setText(elapsedTime);
+
+            String remainTime = createTimeLabel(totalTime - currentPosition);
+            remainingTimeLabel.setText("- " + remainTime);
+        }
+    };
+
+    public String createTimeLabel(int time) {
+        String timeLabel;
+        int min = time / 1000 / 60;
+        int sec = time / 1000 % 60;
+
+        timeLabel = min + ":";
+        if (sec < 10) timeLabel += "0";
+        timeLabel += sec;
+
+        return timeLabel;
+    }
+
+    public void playButtonClick(View view) {
+        if (!mp.isPlaying()) {
+            //Stopping
+            mp.start();
+            playButton.setBackgroundResource(R.drawable.pause_icon);
+        } else {
+            //Playing
+            mp.pause();
+            playButton.setBackgroundResource(R.drawable.play_button);
+        }
+    }
+}
