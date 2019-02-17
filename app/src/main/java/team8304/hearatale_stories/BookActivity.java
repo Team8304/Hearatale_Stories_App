079129bbb6c1fff11cdbe1b7b1fbdb2a878b5241
Manu Poruthoor <manu.poruthoor@gatech.edu>
2019-02-17 16:46:22 -0500

    added scrollable text to each story

--- app/src/main/java/team8304/hearatale_stories/BookActivity.java
+++ app/src/main/java/team8304/hearatale_stories/BookActivity.java
@@ -7,11 +7,16 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class BookActivity extends AppCompatActivity {
 
@@ -33,11 +38,31 @@ public class BookActivity extends AppCompatActivity {
 
         bookTitle = getIntent().getExtras().getString("bookTitle");
         storyContent = (TextView) findViewById(R.id.storyContentTextView);
+        storyContent.setMovementMethod(new ScrollingMovementMethod());
         playButton = (Button) findViewById(R.id.playButton);
         elapsedTimeLabel = (TextView) findViewById(R.id.elapsedTimeLabel);
         remainingTimeLabel = (TextView) findViewById(R.id.remainingTimeLabel);
         Uri bookPath = Uri.parse("android.resource://" + getPackageName() + "/raw/" + ""
             + formatBookTitle(bookTitle));
+        Uri storyContentPath = Uri.parse("android.resource://" + getPackageName() + "/raw/" + "story_"
+                + formatBookTitle(bookTitle));
+//        InputStream inputStream = getResources().openRawResource(R.raw.story_do_you_like_bubbles);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        int i;
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(storyContentPath);
+            i = inputStream.read();
+            while (i != -1)
+            {
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        storyContent.setText(byteArrayOutputStream.toString());
 
         mp = MediaPlayer.create(this, bookPath);
         mp.setLooping(false);
