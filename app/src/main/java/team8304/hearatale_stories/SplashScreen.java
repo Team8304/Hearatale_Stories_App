f1db0ac3ed84712d22e315822d98084b1cb6f18f
HUI LI <554989106@qq.com>
2019-04-21 18:33:35 -0400

    add the missing rumplestiltskin pic

--- app/src/main/java/team8304/hearatale_stories/SplashScreen.java
+++ app/src/main/java/team8304/hearatale_stories/SplashScreen.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Arrays;
 
